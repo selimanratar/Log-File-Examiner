047568bff2517283de874fa74a86f39f1ab20cbe
Selim “Slim Jim” Kuzucu <56355561+selimanratar@users.noreply.github.com>
2020-09-08 18:42:41 +0300

    Adding more comments to explain the code
    Also adding and input line for the name of system log file

--- log_examiner.py
+++ log_examiner.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 
-
-#MacOS update: /var/log/syslog/ is specifically for Linux, change the path to /Library/Logs/ if your machine is running Macintosh
-
-
 import re, operator, os, csv
 
 errors = {}
 users = {}
 
+
+#Creating the regular expressions to look for in the file
 error_pattern = r'ticky: ERROR ([\w\s\']*) \((.+)\)'
 info_pattern = r'ticky: INFO.* \((.+)\)'
 
-with open('syslog.log', 'r') as file:
+
+# MacOS update. Since it is system.log for Mac and syslog.log for Linux and some other names for various Linux distributions, using
+# an input would allow the script to work in different OSs.
+os_name = input("What is your OS?")
+
+
+if os_name == "MacOS":
+  osdependent_log = "system.log"
+elif os_name == "Linux"
+  os_dependent_log = "syslog.log"
+
+else:
+  print("This script can only work for Linux or MacOS")
+
+#Opening and searching the system log file for user activity and error messages
+
+with open(os_dependent_log, 'r') as file:
   for line in file.readlines():
     if re.search(error_pattern, line):
       to_find = re.search(error_pattern, line)
@@ -22,16 +36,21 @@ with open('syslog.log', 'r') as file:
     if re.search(info_pattern, line):
       to_find = re.search(info_pattern, line)
       users.setdefault(to_find.group(1), [0,0])[0]+=1
+
+# Sorting the findings to find the most common errors and most active users      
 error_sorted = sorted(errors.items(), key = operator.itemgetter(1), reverse = True)
 user_sorted = sorted(users.items(), key = operator.itemgetter(1), reverse=True)
 print(error_sorted)
 print(user_sorted)
 
+#Writing the error results to a .csv file
 with open('error_message.csv','w') as output:
   writer = csv.writer(output)
   writer.writerow(['Error','Count'])
   writer.writerows(error_sorted)
 
+  
+#Writing the user results to a .csv file
 with open('user_statistics.csv','w') as output:
   writer = csv.writer(output)
   writer.writerow(['Username','INFO','ERROR'])
